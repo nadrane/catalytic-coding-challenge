14c80aa7252113e6d98a607625cf6541250c89f1
Camden Ko <camdenko@gmail.com>
2017-11-09 17:11:02 -0600

    Molecule NavBar scaffolding

---
+++ client/components/Molecules/NavBar/NavIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import {
+  BoldTitle,
+} from '../../Atoms'
+import Icon from '../../../../public/images/pushbot-peeker.png'
+
+export default () => (
+  <Icon />
+)
