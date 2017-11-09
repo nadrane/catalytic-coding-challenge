0fa8d7ca35f71f903cd38c2b7c3209be9ddeea81
Camden Ko <camdenko@gmail.com>
2017-11-09 17:07:51 -0600

    remvoed react from navtext

--- client/components/Atoms/Typography/NavText.js
+++ client/components/Atoms/Typography/NavText.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Tag = styled.a`
+export default styled.a`
   font-family: Roboto, sans-serif;
 `
-export default props => <Tag href={props.href}>{props.children}</Tag>
+
