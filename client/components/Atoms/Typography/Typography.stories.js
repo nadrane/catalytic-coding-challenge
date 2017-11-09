3289834b2c55d4204c42baeab7bb2b607b92a8fc
Camden Ko <camdenko@gmail.com>
2017-11-09 16:47:30 -0600

    bold title story

--- client/components/Atoms/Typography/Typography.stories.js
+++ client/components/Atoms/Typography/Typography.stories.js
@@ -2,5 +2,8 @@ import React from 'react'
 import { storiesOf } from '@storybook/react'
 
 import {
-
+  BoldTitle,
 } from '../Typography'
+
+storiesOf('Typography/BoldTitle', module)
+  .add('BoldTitle', () => <BoldTitle>I am a bold title!</BoldTitle>)
