13f67e8b2f03cbabb442479101341f22e384924d
Camden Ko <camdenko@gmail.com>
2017-11-10 16:45:49 -0600

    process reducer

---
+++ client/store/proccess.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+const READ_PROCESS = 'READ_PROCESS'
+const UPDATE_PROCESS = 'UPDATE_PROCESS'
+
+const readProcess = proce => ({ type: READ_PROCESS, proce })
+const updateProcess = proce => ({ type: UPDATE_PROCESS, proce })
+
+export const getProcess = () =>
+  async (dispatch) => {
+    try {
+      const response = await axios.get('/api/proccess')
+      dispatch(readProcess(response))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case READ_PROCESS:
+      return action.proce
+    case UPDATE_PROCESS:
+      return Object.assign(state, action.proce)
+    default:
+      return state
+  }
+}
