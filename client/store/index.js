9ddbabb4298083a15eec15edbbe4381ea263f106
Camden Ko <camdenko@gmail.com>
2017-11-10 16:57:59 -0600

    plugged into store

--- client/store/index.js
+++ client/store/index.js
@@ -7,10 +7,14 @@ import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
+import process from './process'
 import steps from './steps'
+import users from './users'
 
 const reducer = combineReducers({
+  process,
   steps,
+  users,
 })
 
 const middleware = composeWithDevTools(applyMiddleware(
@@ -20,4 +24,6 @@ const middleware = composeWithDevTools(applyMiddleware(
 const store = createStore(reducer, middleware)
 
 export default store
+export * from './process'
 export * from './steps'
+export * from './users'
