e43c55c3629c99072464a8ae9f732e1d5a0d5440
Camden Ko <camdenko@gmail.com>
2017-11-10 09:45:56 -0600

    leftbartitle story

---
+++ client/components/Atoms/Typography/LeftBarTitle.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export default styled.h1`
+  margin: 0;
+  font-family: Roboto, sans-serif;
+  font-weight: 300;
+  font-size: 1.6rem;
+`
--- client/components/Atoms/Typography/Typography.stories.js
+++ client/components/Atoms/Typography/Typography.stories.js
@@ -7,6 +7,7 @@ import {
   BoldTitle,
   NavText,
   SmallBody,
+  LeftBarTitle,
   LeftBarText,
 } from '../Typography'
 
@@ -14,9 +15,13 @@ const Black = styled.div`
   background-color: black;
 `
 
+// Text that needs black background
 storiesOf('Atoms/Typography', module)
   .addDecorator(story => <Black>{story()}</Black>)
   .add('BoldTitle', () => <BoldTitle>I am a bold title!</BoldTitle>)
   .add('NavText', () => <NavText>I am a link!</NavText>)
   .add('SmallBody', () => <SmallBody>Small Body Text :(</SmallBody>)
   .add('LeftBarText', () => <LeftBarText>triggers</LeftBarText>)
+
+storiesOf('Atoms/Typography', module)
+  .add('LeftBarTitle', () => <LeftBarTitle>Fill out information</LeftBarTitle>)
