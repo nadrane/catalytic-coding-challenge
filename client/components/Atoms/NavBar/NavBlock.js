474e2a4f3292b82f7987f39e211109a57715a4de
Camden Ko <camdenko@gmail.com>
2017-11-10 09:14:08 -0600

    navblock styling

--- client/components/Atoms/NavBar/NavBlock.js
+++ client/components/Atoms/NavBar/NavBlock.js
@@ -4,9 +4,12 @@ import styled from 'styled-components'
 import {
   NavText,
 } from '../../Atoms'
+import styles from '../../styles'
 
 const Block = styled.div`
   padding: 3rem;
+  background-color: ${styles.colors.dark};
+  width: fit-content;
 `
 
 export default props => (
