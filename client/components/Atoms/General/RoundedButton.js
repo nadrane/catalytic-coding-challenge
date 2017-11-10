83152ea7d4c39ee6761de1059ccb9a65bf4b7da0
Camden Ko <camdenko@gmail.com>
2017-11-10 10:49:18 -0600

    roundedbutton

--- client/components/Atoms/General/RoundedButton.js
+++ client/components/Atoms/General/RoundedButton.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
+import styles from '../../styles'
+
 export default styled.button`
-  padding: 3rem 1rem;
+  padding: .6em 3em;
+  background-color: ${styles.colors.darkAccent};
+  color: white;
+  font-family: Roboto, sans-serif;
+  font-size: 1rem;
+  border-radius: 3em;
 `
