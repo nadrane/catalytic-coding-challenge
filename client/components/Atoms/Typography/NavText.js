20f4715b7d693986c4ca205d2b39b04ee3b9c3c7
Camden Ko <camdenko@gmail.com>
2017-11-10 09:14:37 -0600

    navtext color

--- client/components/Atoms/Typography/NavText.js
+++ client/components/Atoms/Typography/NavText.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
 
+import styles from '../../styles'
+
 export default styled.a`
   font-family: Roboto, sans-serif;
   text-transform: uppercase;
+  color: ${styles.colors.grey};
 `
 
