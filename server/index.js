934a7d8795d0af9382e10027ee64fdbbe1a509cb
Camden Ko <camdenko@gmail.com>
2017-11-10 14:40:39 -0600

    got rid of middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const morgan = require('morgan')
 const express = require('express')
-const bodyParser = require('body-parser')
 const path = require('path')
 const PORT = process.env.PORT || 8080
 
@@ -8,9 +7,6 @@ const app = express()
 module.exports = app
 const createApp = () => {
   app.use(morgan('dev'))
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
-  app.use(express.static(path.join(__dirname, '..', 'node_modules')))
   app.use(express.static(path.join(__dirname, '..', 'public')))
   app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'))
