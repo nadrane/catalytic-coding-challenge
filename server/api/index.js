b5586761772afca710652a7db413060b23dcd48c
Camden Ko <camdenko@gmail.com>
2017-11-10 16:10:17 -0600

    fake api routes work

--- server/api/index.js
+++ server/api/index.js
@@ -16,7 +16,7 @@ router.get('/process', async (req, res, next) => {
 
 router.get('/steps', async (req, res, next) => {
   try {
-    res.json(await promisifiedRF('../data/steps.json'))
+    res.json(await promisifiedRF(path.join(__dirname, '..', 'data/steps.json')))
   } catch (error) {
     next(error)
   }
@@ -24,7 +24,7 @@ router.get('/steps', async (req, res, next) => {
 
 router.get('/steps/:id', async (req, res, next) => {
   try {
-    const steps = await promisifiedRF('../../data/steps.json')
+    const steps = await promisifiedRF(path.json(__dirname, '..', 'data/steps.json'))
     res.json(steps[req.params.id])
   } catch (error) {
     next(error)
@@ -33,7 +33,7 @@ router.get('/steps/:id', async (req, res, next) => {
 
 router.get('/users', async (req, res, next) => {
   try {
-    res.json(await promisifiedRF('../../data/users.json'))
+    res.json(await promisifiedRF(path.join(__dirname, '..', 'data/users.json')))
   } catch (error) {
     next(error)
   }
