1301f9e58a9b71dc73890003f8dfb4ddb01e7d41
Camden Ko <camdenko@gmail.com>
2017-11-10 15:03:45 -0600

    basic reducer

---
+++ client/store/steps.js
@@ -0,0 +1,28 @@
+const CREATE_STEP = 'CREATE_STEP'
+const READ_STEPS = 'READ_STEPS'
+const UPDATE_STEP = 'UPDATE_STEP'
+const DELETE_STEP = 'DELETE_STEP'
+const REORDER_STEP = 'REORDER_STEP'
+
+const createStep = step => ({ type: CREATE_STEP, step })
+const readSteps = steps => ({ type: READ_STEPS, steps })
+const updateStep = step => ({ type: UPDATE_STEP, step })
+const deleteStep = stepId => ({ type: DELETE_STEP, stepId })
+
+export default (state = [], action) => {
+  switch (action.type) {
+    case CREATE_STEP:
+      return [...state, action.step]
+    case READ_STEPS:
+      return action.steps
+    case UPDATE_STEP:
+      return [
+        ...state.filter(step => step.stepNumber !== action.step.stepNumber),
+        action.step,
+      ]
+    case DELETE_STEP:
+      return [...state.filter(step => step.stepNumber !== action.stepId)]
+    default:
+      return state
+  }
+}
